e5d7fb926a714642717bd3f3eb47d09187083fd7
Ppgtjmad <killer2zouk_11@hotmail.fr>
2014-10-19 16:38:06 +0200

    Create commun.hpp

---
+++ dialogues/commun.hpp
@@ -0,0 +1,21 @@
+class survie_RscPicture {
+	shadow = 0;
+	type = 0;
+	style = 48;
+	sizeEx = 0.023;
+	font = "PuristaMedium";
+	colorBackground[] = {};
+	colorText[] = {};
+	x = 0;
+	y = 0;
+	w = 0.2;
+	h = 0.15;
+	tooltipColorText[] = {1,1,1,1};
+	tooltipColorBox[] = {1,1,1,1};
+	tooltipColorShade[] = {0,0,0,0.65};
+};
+
+class survie_RscPictureKeepAspect : survie_RscPicture 
+{
+	style = 0x30 + 0x800;
+};
